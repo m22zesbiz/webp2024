e32fd9263c0caf19bbf50525d559ab1e804dfa06
m22zesbiz <83802103+m22zesbiz@users.noreply.github.com>
2024-03-20 16:47:53 +0800

    Add files via upload

---
+++ lab2/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    var container = document.querySelector('#container'); 
+    var num1 = Math.floor(Math.random() * 2) + 1;
+    for (var i = 0; i < num1; i++) {
+        char = Math.floor(Math.random() * 26) + 97;
+        container.innerHTML += String.fromCharCode(char);
+    }
+
+    window.addEventListener('keyup', (e) => {
+        if (e.key == container.innerHTML.substring(0, 1)){
+            container.innerHTML = container.innerHTML.substring(1, container.innerHTML.length); 
+        }
+        var num2 = Math.floor(Math.random() * 3) + 1;
+        console.log(num2);
+        for (var i = 0; i < num2; i++) {
+            char = Math.floor(Math.random() * 26) + 97;
+            container.innerHTML += String.fromCharCode(char);
+        }
+    })
+})
