7270087e8c06368608b548456f72dc390580911d
m22zesbiz <83802103+m22zesbiz@users.noreply.github.com>
2024-05-01 15:29:21 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,14 @@
+import CGU_Login from './cgu_login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {CGU_Login()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
